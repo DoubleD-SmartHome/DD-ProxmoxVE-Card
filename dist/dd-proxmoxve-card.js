9dbc1afa5a771885b170aeb67843157fad444b2b
DoubleD-SmartHome <136167233+DoubleD-SmartHome@users.noreply.github.com>
2025-03-18 19:06:14 -0400

    Update dd-proxmoxve-card.js

--- dist/dd-proxmoxve-card.js
+++ dist/dd-proxmoxve-card.js
@@ -90,10 +90,10 @@ class DFProxmoxCard extends HTMLElement {
   // The rules for sizing your card in the grid in sections view
   getLayoutOptions() {
     return {
-      grid_rows: 2,
+      grid_rows: 3,
       grid_columns: 4,
-      grid_min_rows: 2,
-      grid_max_rows: 2,
+      grid_min_rows: 3,
+      grid_max_rows: 3,
     };
   }
 
