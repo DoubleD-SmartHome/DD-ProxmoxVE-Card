7961f5608c259df8d85f8b224b1b03b440d6d6a7
DoubleD-SmartHome <136167233+DoubleD-SmartHome@users.noreply.github.com>
2025-03-19 14:50:14 -0400

    Update dd-proxmoxve-card.js

--- dist/dd-proxmoxve-card.js
+++ dist/dd-proxmoxve-card.js
@@ -34,40 +34,40 @@ class DFProxmoxCard extends HTMLElement {
 
     this.content.innerHTML = `
 	<div class="df-proxmox-container">
-	  <div class="Logo">
+	  <div class="grid-item logo">
 	    <div id="logo" title="" class="" style="height: 80%; background: center / contain no-repeat url('/local/community/DD-ProxmoxVE-Card/assets/${LOGO}.png');"></div>
 	  </div>
-	  <div class="Main">
-	    <div class="name">${DEVICE_NAME}</div>
-	    <div class="uptime" title="Started: ${STARTUP}">${UPTIME}</div>
+	  <div class="grid-item main">
+	    <div class="">${DEVICE_NAME}</div>
+	    <div class="" title="Started: ${STARTUP}">${UPTIME}</div>
 	  </div>
-	  <div class="Status">
-	    <div class="MainStatus">
+	  <div class="grid-item status">
+	    <div class="">
 	      <div title="" class="" style="height: 80%; background: center / contain no-repeat url('/local/community/DD-ProxmoxVE-Card/assets/${TYPE}_${STATUS}.png');"></div>
 	    </div>
-	    <div class="SubStatus" style="display: flex; justify-content: center; height:30px;">
+	    <div class="" style="display: flex; justify-content: center; height:30px;">
               <div id="icon-container" style="width: 32px; float: left;"  title="Last Backup:&#013;${SSL_DATE}"><ha-icon icon="mdi:backup-restore" style="color: goldenrod;"></ha-icon></div>
 	      <div id="icon-container" style="width: 32px; float: left;" title="SSL Certificate Expires:&#013;${SSL_DATE}"><ha-icon icon="mdi:certificate" style="color: darkgreen;"></ha-icon></div>
               <div id="icon-container" style="width: 32px; float: left;" title="SSL Certificate Expires:&#013;${SSL_DATE}"><ha-icon icon="mdi:console" style="color: darkgreen;"></ha-icon></div>
             </div>
 	  </div>
-	  <div class="Stats Stat1 df-dark_supported">
+	  <div class="grid-item stat1 df-dark_supported">
             <div class="RAM"></div>
             <div class="STAT_VALUE df-dark_supported">${RAM}%</div>
 	  </div>
-	  <div class="Stats Stat2 df-dark_supported">
+	  <div class="grid-item stat2 df-dark_supported">
    	    <div class="STAT_VALUE df-dark_supported">${RAM}%</div>
           </div>
-	  <div class="Stats Stat3 df-dark_supported">
+	  <div class="grid-item stat3 df-dark_supported">
             <div class="STAT_VALUE df-dark_supported">${RAM}%</div>
 	  </div>
-	  <div class="Stats Stat4 df-dark_supported">
+	  <div class="grid-item stat4 df-dark_supported">
             <div class="STAT_VALUE df-dark_supported">${RAM}%</div>
           </div>
-	  <div class="Stats Stat5 df-dark_supported">
+	  <div class="grid-item stat5 df-dark_supported">
             <div class="STAT_VALUE df-dark_supported">${RAM}%</div>
           </div>
-	  <div class="Stats Stat6 df-dark_supported">
+	  <div class="grid-item stat6 df-dark_supported">
             <div class="STAT_VALUE df-dark_supported">${RAM}%</div>
           </div>
 	</div>
