f7cbfcdc50fc23300de4ba9c570d14f07e568b7a
DoubleD-SmartHome <136167233+DoubleD-SmartHome@users.noreply.github.com>
2025-03-19 14:55:54 -0400

    Update dd-proxmoxve-card.js

--- dist/dd-proxmoxve-card.js
+++ dist/dd-proxmoxve-card.js
@@ -52,8 +52,7 @@ class DFProxmoxCard extends HTMLElement {
             </div>
 	  </div>
 	  <div class="grid-item stat1 df-dark_supported">
-            <div class="RAM"></div>
-            <div class="STAT_VALUE df-dark_supported">${RAM}%</div>
+            S1
 	  </div>
 	  <div class="grid-item stat2 df-dark_supported">
    	    <div class="STAT_VALUE df-dark_supported">${RAM}%</div>
