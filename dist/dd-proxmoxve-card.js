62eba57f4eca25b0414b423101b9ed80b7de84fc
DoubleD-SmartHome <136167233+DoubleD-SmartHome@users.noreply.github.com>
2025-03-20 18:10:25 -0400

    Update dd-proxmoxve-card.js

--- dist/dd-proxmoxve-card.js
+++ dist/dd-proxmoxve-card.js
@@ -73,7 +73,7 @@ class DFProxmoxCard extends HTMLElement {
             <button class="button" click=${this._press}>
               Shutdown
             </button>
-	    <div id="shutdown" title="" class="button">Shutdown6</div>
+	    <div id="shutdown" title="" class="button">Shutdown7</div>
             Actions Coming Soon...
 	  </div>
 	</div>
