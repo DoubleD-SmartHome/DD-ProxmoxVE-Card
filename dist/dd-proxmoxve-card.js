7fbedfe3f8a2a5c8b124c83d2462b0986031f082
DoubleD-SmartHome <136167233+DoubleD-SmartHome@users.noreply.github.com>
2025-03-21 09:44:15 -0400

    Update dd-proxmoxve-card.js

--- dist/dd-proxmoxve-card.js
+++ dist/dd-proxmoxve-card.js
@@ -104,9 +104,9 @@ console.log("Attributes for entity:", hass.states[entityId].attributes);
             <div class="STAT_VALUE df-dark_supported">${RAM}%</div>
           </div>
 	  <div class="grid-item actions">
-            <button id="shutdown" title="" class="button"><ha-icon icon="mdi:backup-pause" style="color: goldenrod;"></button>
-	    <button id="shutdown" title="" class="button"><ha-icon icon="mdi:backup-stop" style="color: goldenrod;"></button>
-            <button id="shutdown" title="" class="button"><ha-icon icon="mdi:backup-start" style="color: goldenrod;"></button>
+            <div id="icon-container" style="width: 32px; float: left;"  title="Last Backup:&#013;${SSL_DATE}"><ha-icon icon="mdi:pause" style="color: goldenrod;"></ha-icon></div>
+	    <div id="icon-container" style="width: 32px; float: left;"  title="Last Backup:&#013;${SSL_DATE}"><ha-icon icon="mdi:stop" style="color: goldenrod;"></ha-icon></div>
+            <button id="shutdown" title="" class="button"><ha-icon icon="mdi:start" style="color: goldenrod;"></button>
 	  </div>
 	</div>
     `;
