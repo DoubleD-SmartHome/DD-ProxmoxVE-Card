28d65e754af19c1c5421d04be91b8808baeca3df
DoubleD-SmartHome <136167233+DoubleD-SmartHome@users.noreply.github.com>
2025-03-21 12:39:16 -0400

    Update dd-proxmoxve-card.js

--- dist/dd-proxmoxve-card.js
+++ dist/dd-proxmoxve-card.js
@@ -107,7 +107,7 @@ console.log("Attributes for entity:", hass.states[entityId].attributes);
    		Actions:
           </div>
 	  <div class="grid-item actions">
-            <button id="ActionPause" title="pause" class="button">2<ha-icon icon="mdi:pause" style="color: goldenrod;"></ha-icon></button>
+            <button id="ActionPause" title="pause" class="button">3<ha-icon icon="mdi:pause" style="color: goldenrod;"></ha-icon></button>
 	    <button id="ActionStop" title="Stop" class="button"><ha-icon icon="mdi:stop" style="color: goldenrod;"></ha-icon></button>
             <button id="ActionShutdown" title="Shutdown" class="button"><ha-icon icon="mdi:play" style="color: goldenrod;"></ha-icon></button>
 	  </div>
@@ -116,8 +116,8 @@ console.log("Attributes for entity:", hass.states[entityId].attributes);
 
 	const actionButtons = this.querySelectorAll('[id^="Action"]');
 	actionButtons.forEach((actionButton) => {
-	  actionButton.addEventListener('click', () => {
-	    if (confirm(this.getAttribute('title')) == true) {
+	  actionButton.addEventListener('click', (event) => {
+	    if (confirm(event.target.title) == true) {
 	      hass.callService('button', 'press', {
 	        entity_id: this.config.shutdown
 	      });
