e60712f79e32eed0d03913ca303cbf9e56bf37bc
DoubleD-SmartHome <136167233+DoubleD-SmartHome@users.noreply.github.com>
2025-03-21 16:09:21 -0400

    Update dd-proxmoxve-card.js

--- dist/dd-proxmoxve-card.js
+++ dist/dd-proxmoxve-card.js
@@ -33,159 +33,106 @@ class DFProxmoxCard extends HTMLElement {
 		const SSL_EXP_DAYS = Math.floor(SSL_EXP_SECONDS / (1000 * 60 * 60 * 24))
 		const SSL_STATUS = "red";
 		
-//const entities = hass.states;
-//console.log(entities); // This will log the list of entities
-
-		//const deviceId = this.config.device;
-		//const entities = Object.keys(hass.states).filter((entityId) => {
-		//	const entity = hass.states[entityId];
-		//	return (
-		//		entity.attributes.device_id === deviceId && entityId.startsWith("button.")
-		//	);
-		//});
-
-//const deviceEntities = Object.keys(hass.states).filter(
-//  (entityId) => hass.states[entityId].attributes.device_id === "b87ccacf7febea6302bc90be98592cc4"
-//);
-//console.log('Device:', "b87ccacf7febea6302bc90be98592cc4");		
-//console.log('Entities for device:', deviceEntities);
-//console.log('get Dev_id:', hass.states['sensor.'+this.config.device+'_disk_used_percentage'].attributes.device_id);
-
-//const entityId = "b87ccacf7febea6302bc90be98592cc4"; // Replace with your entity ID
-//const deviceId = hass.states[this.config.ssl].attributes.device_id;
-
-const entityId = 'sensor.lxc_base_local_101_cpu_used'; // Replace with your entity ID
-const deviceId = hass.states[entityId].attributes.device_id;
-
-console.log(`Device ID for '${entityId}':`, deviceId);
-console.log("Attributes for entity:", hass.states[entityId].attributes);
-//console.log(`work Device ID for entity ${this.config.ssl}: ${deviceId}`);
-		
-//if (deviceId) {
-//  console.log(`Device ID for entity ${entityId}: ${deviceId}`);
-//} else {
-//  console.log(`Device ID not found for entity ${entityId}`);
-//}
-
-		
-    this.content.innerHTML = `
-	 <div class="df-proxmox-container">
-	  <div class="grid-item logo" style="height: 80%; background: center / contain no-repeat url('/local/community/DD-ProxmoxVE-Card/assets/${LOGO}.png');"></div>
-	  <div class="grid-item main no-overflow">
-	    <div class="no-overflow">${DEVICE_NAME}</div>
-	    <div class="no-overflow" title="Started: ${STARTUP}">${UPTIME}</div>
-	  </div>
-	  <div class="grid-item status">
-	    <div class="${STATUS}" style="display: flex; justify-content: center; height:30px;">
-	      <div title="${STATUS}" class="" style="height: 80%; width: 80%; background: center / contain no-repeat url('/local/community/DD-ProxmoxVE-Card/assets/${TYPE}_${STATUS}.png');"></div>
-	    </div>
-	    <div class="" style="display: flex; justify-content: center; height:30px;">
-              <div id="icon-container" style="width: 32px; float: left;"  title="Last Backup:&#013;${SSL_DATE}"><ha-icon icon="mdi:backup-restore" style="color: goldenrod;"></ha-icon></div>
-	      <div id="icon-container" style="width: 32px; float: left;" title="SSL Certificate Expires:&#013;${SSL_DATE}"><ha-icon icon="mdi:certificate" style="color: darkgreen;"></ha-icon></div>
-              <div id="icon-container" style="width: 32px; float: left;" title="SSL Certificate Expires:&#013;${SSL_DATE}"><ha-icon icon="mdi:console" style="color: darkgreen;"></ha-icon></div>
-            </div>
-	  </div>
-	  <div class="grid-item stat1 df-dark_supported">
-            <div class="STAT_VALUE df-dark_supported">S1i</div>
-	  </div>
-	  <div class="grid-item stat2 df-dark_supported">
-   	    <div class="STAT_VALUE df-dark_supported">${RAM}%</div>
-          </div>
-	  <div class="grid-item stat3 df-dark_supported">
-            <div class="STAT_VALUE df-dark_supported">${RAM}%</div>
-	  </div>
-	  <div class="grid-item stat4 df-dark_supported">
-            <div class="STAT_VALUE df-dark_supported">${RAM}%</div>
-          </div>
-	  <div class="grid-item stat5 df-dark_supported">
-            <div class="STAT_VALUE df-dark_supported">${RAM}%</div>
-          </div>
-	  <div class="grid-item stat6 df-dark_supported">
-            <div class="STAT_VALUE df-dark_supported">${RAM}%</div>
-          </div>
-	  <div class="grid-item actionlabel">
-   		Actions:
-          </div>
-	  <div class="grid-item actions">
-            <button id="ActionShutdown" title="Start" class="button" ${STATUS == 'on' ? 'disabled' : ''}><ha-icon icon="mdi:play"></ha-icon></button>
-	    <button id="ActionStop" title="Stop" class="button" ${STATUS == 'on' ? '' : 'disabled'}><ha-icon icon="mdi:stop"></ha-icon></button>
-	    <button id="ActionShutdown" title="Shutdown" class="button" ${STATUS == 'on' ? '' : 'disabled'}><ha-icon icon="mdi:power"></ha-icon></button>
-            <button id="ActionReboot" title="Reboot" class="button" ${STATUS == 'on' ? '' : 'disabled'}><ha-icon icon="mdi:restart"></ha-icon></button>
-	  </div>
-	</div>
-    `;
-
-	const actionButtons = this.querySelectorAll('[id^="Action"]');
-	actionButtons.forEach((actionButton) => {
-	  actionButton.addEventListener('click', (event) => {
-	    const actionid = 'button.'+this.config.device+'_'+event.currentTarget.getAttribute('title');
-	    if (confirm("Event: "+actionid)) == true) {
-	      hass.callService('button', 'press', {
-	        entity_id: button.lxc_base_local_101_stop
-	      });
-            }
-	  });
-	});
-		
+		this.content.innerHTML = `
+  			<div class="df-proxmox-container">
+				<div class="grid-item logo" style="height: 80%; background: center / contain no-repeat url('/local/community/DD-ProxmoxVE-Card/assets/${LOGO}.png');"></div>
+				<div class="grid-item main no-overflow">
+					<div class="no-overflow">${DEVICE_NAME}</div>
+					<div class="no-overflow" title="Started: ${STARTUP}">${UPTIME}</div>
+				</div>
+				<div class="grid-item status">
+					<div class="${STATUS}" style="display: flex; justify-content: center; height:30px;">
+						<div title="${STATUS}" class="" style="height: 80%; width: 80%; background: center / contain no-repeat url('/local/community/DD-ProxmoxVE-Card/assets/${TYPE}_${STATUS}.png');"></div>
+					</div>
+					<div class="" style="display: flex; justify-content: center; height:30px;">
+						<div id="icon-container" style="width: 32px; float: left;"  title="Last Backup:&#013;${SSL_DATE}"><ha-icon icon="mdi:backup-restore" style="color: goldenrod;"></ha-icon></div>
+						<div id="icon-container" style="width: 32px; float: left;" title="SSL Certificate Expires:&#013;${SSL_DATE}"><ha-icon icon="mdi:certificate" style="color: darkgreen;"></ha-icon></div>
+						<div id="icon-container" style="width: 32px; float: left;" title="SSL Certificate Expires:&#013;${SSL_DATE}"><ha-icon icon="mdi:console" style="color: darkgreen;"></ha-icon></div>
+					</div>
+				</div>
+				<div class="grid-item stat1 df-dark_supported">
+					<div class="STAT_VALUE df-dark_supported">S1i</div>
+				</div>
+				<div class="grid-item stat2 df-dark_supported">
+					<div class="STAT_VALUE df-dark_supported">${RAM}%</div>
+				</div>
+				<div class="grid-item stat3 df-dark_supported">
+					<div class="STAT_VALUE df-dark_supported">${RAM}%</div>
+				</div>
+				<div class="grid-item stat4 df-dark_supported">
+					<div class="STAT_VALUE df-dark_supported">${RAM}%</div>
+				</div>
+				<div class="grid-item stat5 df-dark_supported">
+					<div class="STAT_VALUE df-dark_supported">${RAM}%</div>
+				</div>
+				<div class="grid-item stat6 df-dark_supported">
+					<div class="STAT_VALUE df-dark_supported">${RAM}%</div>
+				</div>
+				<div class="grid-item actionlabel">
+					Actions:
+				</div>
+				<div class="grid-item actions">
+					<button id="ActionShutdown" title="Start" class="button" ${STATUS == 'on' ? 'disabled' : ''}><ha-icon icon="mdi:play"></ha-icon></button>
+					<button id="ActionStop" title="Stop" class="button" ${STATUS == 'on' ? '' : 'disabled'}><ha-icon icon="mdi:stop"></ha-icon></button>
+					<button id="ActionShutdown" title="Shutdown" class="button" ${STATUS == 'on' ? '' : 'disabled'}><ha-icon icon="mdi:power"></ha-icon></button>
+					<button id="ActionReboot" title="Reboot" class="button" ${STATUS == 'on' ? '' : 'disabled'}><ha-icon icon="mdi:restart"></ha-icon></button>
+				</div>
+			</div>
+		`;
   	}
 
-	_press(ev) {
-        	hass.callService('button', 'press', {
-			entity_id: this.config.shutdown
-		});
-	}
 
-	  	// The user supplied configuration. Throw an exception and Home Assistant
-	  	// will render an error card.
-	  	setConfig(config) {
-	  	  //if (!config.load_entity) {
-	    	  //  throw new Error("You need to define an entity2");
-		    //}
-		    this.config = config;
-		  }
-	
-		  // The height of your card. Home Assistant uses this to automatically
-		  // distribute all cards over the available columns in masonry view
-		  getCardSize() {
-		    return 4;
-		  }
-		
-		  // The rules for sizing your card in the grid in sections view
-		  getLayoutOptions() {
-		    return {
-		      grid_rows: 3,
-		      grid_columns: 4,
-		      grid_min_rows: 3,
-		      grid_max_rows: 3,
-		    };
-		  }
-		
-		static getStubConfig() {
-	    		return { device: "lxc_name_number" }
-	  	}
+	// The user supplied configuration. Throw an exception and Home Assistant
+	// will render an error card.
+	setConfig(config) {
+		//if (!config.load_entity) {
+		//  throw new Error("You need to define an entity2");
+		//}
+		this.config = config;
 	}
+	
+	// The height of your card. Home Assistant uses this to automatically
+	// distribute all cards over the available columns in masonry view
+	getCardSize() {
+		return 4;
+	}
+		
+	// The rules for sizing your card in the grid in sections view
+	getLayoutOptions() {
+		return {
+			grid_rows: 3,
+			grid_columns: 4,
+			grid_min_rows: 3,
+			grid_max_rows: 3,
+		};
+	}
+		
+	static getStubConfig() {
+		return { device: "lxc_name_number" }
+	}
+}
 
 customElements.define("df-proxmox-card", DFProxmoxCard);
 
 // Add card type to the Home Assistant card registry
 window.customCards = window.customCards || [];
 window.customCards.push({
-  type: 'df-proxmox-card',
-  name: 'DD Proxmox VE Card',
-  description: 'a DoubleD Proxmox VE Card for Container and VMs.',
-  preview: true,
+	type: 'df-proxmox-card',
+	name: 'DD Proxmox VE Card',
+	description: 'a DoubleD Proxmox VE Card for Container and VMs.',
+	preview: true,
 });
 
-  function calculateTimeDifference(startDate, endDate) {
-    // Convert dates to milliseconds
-    const start = new Date(startDate).getTime();
-    const end = new Date(endDate).getTime();
-    // Calculate the difference in milliseconds
-    const difference = end - start;
-    // Calculate days, hours, minutes, and seconds
-    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
-    return { days, hours, minutes, seconds };
-  }
+function calculateTimeDifference(startDate, endDate) {
+	// Convert dates to milliseconds
+	const start = new Date(startDate).getTime();
+	const end = new Date(endDate).getTime();
+	// Calculate the difference in milliseconds
+	const difference = end - start;
+	// Calculate days, hours, minutes, and seconds
+	const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+	const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+	const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+	return { days, hours, minutes, seconds };
+}
