bd6c6acadb22209740a4300daee3aa291ec4a2fe
DoubleD-SmartHome <136167233+DoubleD-SmartHome@users.noreply.github.com>
2025-03-22 11:43:28 -0400

    Update dd-proxmoxve-card.js

--- dist/dd-proxmoxve-card.js
+++ dist/dd-proxmoxve-card.js
@@ -1,7 +1,7 @@
 class DFProxmoxCard extends HTMLElement {
 	// 2025-03-19 @ 2:23pm
 	set hass(hass) {
-		const VERSION="0.00.054";
+		const VERSION="0.00.055";
 		if (!this.content) {
 			this.innerHTML = `
 				<link type="text/css" rel="stylesheet" href="/local/community/DD-ProxmoxVE-Card/dd-proxmoxve-card.css">
@@ -69,7 +69,7 @@ class DFProxmoxCard extends HTMLElement {
 
 		stats.forEach((stat) => {
 			myHTML += `
-   				<div class="stat borderRed">
+   				<div class="stat borderRed" style="width: 200px; float: left;">
 	   				<div class="stat_label">1</div>
 					<div class="stat_value">2</div>
      				</div>
