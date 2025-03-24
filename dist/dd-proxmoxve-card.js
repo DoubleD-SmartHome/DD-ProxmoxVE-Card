0ccca47dd114a99d71b6e2795a0fc7b8bdb8e2da
DoubleD-SmartHome <136167233+DoubleD-SmartHome@users.noreply.github.com>
2025-03-24 14:52:35 -0400

    Update dd-proxmoxve-card.js

--- dist/dd-proxmoxve-card.js
+++ dist/dd-proxmoxve-card.js
@@ -1,7 +1,6 @@
 class DFProxmoxCard extends HTMLElement {
-	// 2025-03-19 @ 2:23pm
 	set hass(hass) {
-		const VERSION="0.00.106";
+		const VERSION="0.00.107";
 		if (!this.content) {
 			this.innerHTML = `
 				<link type="text/css" rel="stylesheet" href="/local/community/DD-ProxmoxVE-Card/dd-proxmoxve-card.css">
@@ -76,14 +75,14 @@ class DFProxmoxCard extends HTMLElement {
 			let SSL_EXP_DAYS = Math.round(SSL_EXP_SECONDS / (1000 * 60 * 60 * 24)*10)/10;
 			let SSL_COLOR;
 			switch(true) {
-				case (SSL_EXP_DAYS <= 0 ):
-					SSL_COLOR = "darkred";
-    					break;
-  				case (SSL_EXP_DAYS <= 7):
+				case (SSL_EXP_DAYS > 7):
+					SSL_COLOR = "darkgreen";
+					break;
+  				case (SSL_EXP_DAYS > 0 && <= SSL_EXO_DAYS < 7):
     					SSL_COLOR = "goldenrod";
    					break;
  				 default:
-					SSL_COLOR = "darkgreen";
+					SSL_COLOR = "darkred";
 			}
 			myHTML += `<div id="icon-container" style="width: 32px; float: left;" title="SSL Certificate Expires:&#013;${SSL_DATE}&#013;Expires in ${SSL_EXP_DAYS} days"  onclick="alert(this.getAttribute('title'))"><ha-icon icon="mdi:certificate" style="color: ${SSL_COLOR};"></ha-icon></div>`;
 		}
