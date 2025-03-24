86d91fa6cf1d848c36f9839713edab8ccbd75698
DoubleD-SmartHome <136167233+DoubleD-SmartHome@users.noreply.github.com>
2025-03-24 13:33:41 -0400

    Update dd-proxmoxve-card.js

--- dist/dd-proxmoxve-card.js
+++ dist/dd-proxmoxve-card.js
@@ -1,7 +1,7 @@
 class DFProxmoxCard extends HTMLElement {
 	// 2025-03-19 @ 2:23pm
 	set hass(hass) {
-		const VERSION="0.00.101";
+		const VERSION="0.00.102";
 		if (!this.content) {
 			this.innerHTML = `
 				<link type="text/css" rel="stylesheet" href="/local/community/DD-ProxmoxVE-Card/dd-proxmoxve-card.css">
@@ -46,18 +46,19 @@ class DFProxmoxCard extends HTMLElement {
 
 		if (this.config.backup) {
 			let BACKUP_STATUS = hass.states[this.config.backup] ? hass.states[this.config.backup].state : "unavailable";
+			let BACKUP_DATE = hass.states[this.config.backup] ? hass.states[this.config.backup].attribute.datetime : "unavailable";
 			let BACKUP_COLOR;
 			switch(BACKUP_STATUS) {
-				case "ERROR":
-					BACKUP_COLOR = "darkred";
+				case "SUCCESSFUL":
+					BACKUP_COLOR = "darkgreen";
     					break;
   				case "FAILED":
     					BACKUP_COLOR = "goldenrod";
    					break;
  				 default:
-					BACKUP_COLOR = "darkgreen";
+					BACKUP_COLOR = "darkred";
 			}
-			myHTML += `<div id="icon-container" style="width: 32px; float: left;"  title="Last Backup:&#013;${BACKUP_STATUS}"><ha-icon icon="mdi:backup-restore" style="color: ${BACKUP_COLOR};"></ha-icon></div>`;
+			myHTML += `<div id="icon-container" style="width: 32px; float: left;"  title="Last Backup:&#013;${BACKUP_DATE}"><ha-icon icon="mdi:backup-restore" style="color: ${BACKUP_COLOR};"></ha-icon></div>`;
 		}
 
 		if (this.config.ssl) {
