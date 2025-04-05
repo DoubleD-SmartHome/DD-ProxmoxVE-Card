5ae7b2797d5ab4e04ac9521a6b9fcbeb216a1993
DoubleD-SmartHome <136167233+DoubleD-SmartHome@users.noreply.github.com>
2025-04-05 07:46:56 -0400

    Update dd-proxmoxve-card.js

--- dist/dd-proxmoxve-card.js
+++ dist/dd-proxmoxve-card.js
@@ -1,6 +1,6 @@
 class DFProxmoxCard extends HTMLElement {
 	set hass(hass) {
-		const VERSION="0.00.116";
+		const VERSION="0.00.117";
 		if (!this.content) {
 			this.innerHTML = `
 				<link type="text/css" rel="stylesheet" href="/local/community/DD-ProxmoxVE-Card/dd-proxmoxve-card.css">
@@ -50,7 +50,7 @@ class DFProxmoxCard extends HTMLElement {
 			let BACKUP_DATE = hass.states[this.config.backup] ? new Date(hass.states[this.config.backup].attributes.datetime) : "unavailable";
 			let BACKUP_SECONDS = Math.abs(TODAY_DATE - BACKUP_DATE);
 			let BACKUP_DAYS = Math.round(BACKUP_SECONDS / (1000 * 60 * 60 * 24)*10)/10;
-			let BACKUP_RUNTIME =  hass.states[this.config.backup] ? new Date(hass.states[this.config.backup].attributes.runtime) : "unavailable";
+			let BACKUP_RUNTIME =  hass.states[this.config.backup] ? hass.states[this.config.backup].attributes.runtime : "unavailable";
 			let BACKUP_COLOR;
 			switch(BACKUP_STATUS) {
 				case "SUCCESSFUL":
